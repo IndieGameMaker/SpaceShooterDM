0adac62b82c226dc1a5090170144f75587ca3543
Lee JaeHyun <myleje@gmail.com>
2020-08-03 14:33:30 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -18,6 +18,18 @@ public class PlayerCtrl : MonoBehaviour
         Debug.Log("h=" + h); //Console View에 문자열을 출력
         Debug.Log($"v={v}"); //Debug.Log("v=" + v);
 
-        transform.position += new Vector3(0, 0, 0.1f);
+        //transform.position += new Vector3(0, 0, 0.1f);
+        //transform.position += Vector3.forward * 0.1f;
+        /* Normalized Vector (정규화 벡터)
+            Vector3.forward = Vector3(0, 0, 1)
+            Vector3.up      = Vector3(0, 1, 0)
+            Vector3.right   = Vector3(1, 0, 0)
+
+            Vector3.zero    = Vector3(0, 0, 0)
+            Vector3.one     = Vector3(1, 1, 1)
+        */
+
+        transform.Translate(Vector3.forward * 0.1f * v);
+
     }
 }
