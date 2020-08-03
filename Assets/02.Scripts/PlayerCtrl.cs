8315dbc742474576a91bc2bbb061cf8a57e97d2c
Lee JaeHyun <myleje@gmail.com>
2020-08-03 14:13:31 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -12,9 +12,10 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");  //-1.0f ~ 0.0f ~ 1.0f
+        float v = Input.GetAxis("Vertical");    //-1.0f ~ 0.0f ~ 1.0f
 
         Debug.Log("h=" + h); //Console View에 문자열을 출력
+        Debug.Log($"v={v}"); //Debug.Log("v=" + v);
     }
 }
