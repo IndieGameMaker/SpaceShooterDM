7529f09a35fabbc8b588c025a844bb7a2faa0ad3
Lee JaeHyun <myleje@gmail.com>
2020-08-03 16:38:10 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -38,26 +38,29 @@ public class PlayerCtrl : MonoBehaviour
         float v = Input.GetAxis("Vertical");    //-1.0f ~ 0.0f ~ 1.0f
         float r = Input.GetAxis("Mouse X");
 
-        // Debug.Log("h=" + h); //Console View에 문자열을 출력
-        // Debug.Log($"v={v}"); //Debug.Log("v=" + v);
-
-        //transform.position += new Vector3(0, 0, 0.1f);
-        //transform.position += Vector3.forward * 0.1f;
-        /* Normalized Vector (정규화 벡터)
-            Vector3.forward = Vector3(0, 0, 1)
-            Vector3.up      = Vector3(0, 1, 0)
-            Vector3.right   = Vector3(1, 0, 0)
-
-            Vector3.zero    = Vector3(0, 0, 0)
-            Vector3.one     = Vector3(1, 1, 1)
-        */
-
         Vector3 dir = (Vector3.forward * v) + (Vector3.right * h);
         transform.Translate(dir.normalized * Time.deltaTime * moveSpeed);
         transform.Rotate(Vector3.up * r * Time.deltaTime * turnSpeed);
 
-        // transform.Translate(Vector3.forward * 0.1f * v);
-        // transform.Translate(Vector3.right * 0.1f * h);
-
+        if (v >= 0.1f) //전진
+        {
+            anim.CrossFade(playerAnim.runForward.name, 0.3f);
+        }
+        else if (v <= -0.1f) //후진
+        {
+            anim.CrossFade(playerAnim.runBackward.name, 0.3f);
+        }
+        else if (h >= 0.1f)  //오른쪽
+        {
+            anim.CrossFade(playerAnim.runRight.name, 0.3f);
+        }
+        else if (h <= -0.1f) //왼쪽
+        {
+            anim.CrossFade(playerAnim.runLeft.name, 0.3f);
+        }
+        else
+        {
+            anim.CrossFade(playerAnim.idle.name, 0.3f);
+        }
     }
 }
