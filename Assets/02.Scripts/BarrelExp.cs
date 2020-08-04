650296728eb26c6e50d53f26cfbbe91374361a4e
Lee JaeHyun <myleje@gmail.com>
2020-08-04 12:35:59 +0900

    .

---
+++ Assets/02.Scripts/BarrelExp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelExp : MonoBehaviour
+{
+    private int hitCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            hitCount = hitCount + 1; //++hitCount
+            if (hitCount == 3)
+            {
+
+            }
+        }
+    }
+
+    
+}
