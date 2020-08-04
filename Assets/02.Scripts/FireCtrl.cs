89f6e0d58e5c5d5d69ee2c80e3eb90b4d284ce06
Lee JaeHyun <myleje@gmail.com>
2020-08-04 17:28:43 +0900

    .

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -18,15 +18,30 @@ public class FireCtrl : MonoBehaviour
     void Start()
     {
         audio = GetComponent<AudioSource>();
+        muzzleFlash.enabled = false;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0) == true)
         {
-            //Instantiate(생성할객체, 위치, 각도)
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
-            audio.PlayOneShot(fireSfx, 0.8f);
+            Fire();
         }        
+    }
+
+    void Fire()
+    {
+        //Instantiate(생성할객체, 위치, 각도)
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        audio.PlayOneShot(fireSfx, 0.8f);
+
+        StartCoroutine(ShowMuzzleFlash());
+    }
+
+    IEnumerator ShowMuzzleFlash()
+    {
+        muzzleFlash.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        muzzleFlash.enabled = false;
     }
 }
