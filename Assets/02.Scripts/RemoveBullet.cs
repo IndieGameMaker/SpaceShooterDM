5d481a62c1d3370936be4e53585083418515c09e
Lee JaeHyun <myleje@gmail.com>
2020-08-05 11:41:52 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -8,12 +8,15 @@ public class RemoveBullet : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        // if (coll.gameObject.tag == "BULLET")
-        // {
-        //     Destroy(coll.gameObject);
-        // }
         if (coll.collider.CompareTag("BULLET"))
         {
+            ContactPoint[] cp = coll.contacts;
+            Vector3 _normal = cp[0].normal;    //법선 벡터
+            Vector3 _pos    = cp[0].point;     //충동 위치
+
+            Quaternion rot = Quaternion.LookRotation(_normal);
+            Instantiate(sparkEffect, _pos, rot);
+
             Destroy(coll.gameObject);
         }
     }
