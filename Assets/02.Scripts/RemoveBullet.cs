cbbe70cef431e792ab6a492f12fe31cc8fe64996
Lee JaeHyun <myleje@gmail.com>
2020-08-05 12:17:27 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -13,7 +13,7 @@ public class RemoveBullet : MonoBehaviour
         {
             ContactPoint[] cp = coll.contacts;
             Vector3 _normal = cp[0].normal * ((isBlood) ? -1:1);    //법선 벡터
-            Vector3 _pos    = cp[0].point;     //충동 위치
+            Vector3 _pos    = cp[0].point;     //충돌 위치
 
             Quaternion rot = Quaternion.LookRotation(_normal);
             var effect = Instantiate(sparkEffect, _pos, rot);
